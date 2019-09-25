663ab11e23f9daf79d3c06eee46c5308f019fb5c
tom507ff <54157469+tom507ff@users.noreply.github.com>
2019-09-24 17:26:23 -0700

    Update archivo.php

--- archivo.php
+++ archivo.php
@@ -1,18 +1,18 @@
 <?php
 // esta es una conexion a la base de datos o&M
-
+// variable= tipo db=nombre de base de datos; host=direccion donde se aloja la base de datos
 $dsn = 'mysql:dbname=oym;host=192.1.202.248';
-$user= 'xxx';
-$password= 'xxx';
+$user= 'xxx';  // usuario que ocupas para la conexion
+$password= 'xxx'; // password algunas veces no tienen password quedaria vacia ''
 
-try 
+try                   // ejecutamos el comando try que tratara de ejecutar lo que esta en {}
   {
    $pdo = new PDO ( $dsn,
                     $user,
                     $password);
-  //echo 'conexion exitosa';
-  }catch ( PDOExeption $e )  
+  echo 'conexion exitosa';  // si la conexion es exitosa te mandara un mensaje de 'conexion exitosa'  
+  }catch ( PDOExeption $e )   // aqui atrapamos los errores de conexion 
   {
-  echo 'error al conectarnos: '.$e->getMessage();
+  echo 'error al conectarnos: '.$e->getMessage(); // desplejamos los errores de conexion
   }
 ?>
